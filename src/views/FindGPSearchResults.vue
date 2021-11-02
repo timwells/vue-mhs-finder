1732cce4a6ed5fc6900ebe3ed7bbdbcf2ac19b01
timwells <timwells018@googlemail.com>
2021-11-02 20:19:34 +0000

    API perf section, router tweaks

---
+++ src/views/FindGPSearchResults.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="nhsuk-width-container">
+    <main id="main-content" class="nhsuk-main-wrapper">
+      <div class="nhsuk-u-reading-width">
+        <div class="nhsuk-back-link">
+          <router-link to="/find-gp">
+            <svg
+              class="nhsuk-icon nhsuk-icon__chevron-left"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              aria-hidden="true">
+              <path
+                d="M8.5 12c0-.3.1-.5.3-.7l5-5c.4-.4 1-.4 1.4 0s.4 1 0 1.4L10.9 12l4.3 4.3c.4.4.4 1 0 1.4s-1 .4-1.4 0l-5-5c-.2-.2-.3-.4-.3-.7z"
+              > </path>
+            </svg>
+          </router-link>
+        </div>
+
+        <div v-if="gpSearchResults">
+          <h1>Select your GP to see available services</h1>
+          <h2 class="nhsuk-body-l">
+            <span role="text">
+              We found <span class="results__count">{{ gpSearchResults.length }}</span> GP surgeries using '{{gpSearchTerm}}'.
+            </span>
+          </h2>
+          <p role="text">If your surgery is not here, check your spelling and <a class="results__search__again" href="/find-gp">search again</a>.</p>
+          <div>
+            <ol class="nhsuk-list inline-list">
+              <li v-for="gp in gpSearchResults"
+                :key="gp.SearchKey"
+                class="nhsuk-c-panel results__item"
+                data-index="1">
+                <hr />
+                <div>
+                  <h3 class="results__name">
+                    <span role="text">{{ gp.OrganisationName }}</span>
+                  </h3>
+                  <div >
+                  <p style="display: inline-block; justify-content: space-between;" v-if="gp.Address1">
+                      {{gp.Address1}},
+                  </p>
+                  <p style="display: inline-block; justify-content: space-between;" v-if="gp.Address2">
+                       {{gp.Address2}},
+                  </p>
+                  <p style="display: inline-block; justify-content: space-between;" v-if="gp.Address3">
+                       {{gp.Address3}},
+                  </p>
+                  <p style="display: inline-block; justify-content: space-between;" v-if="gp.City">
+                       {{gp.City}},
+                  </p>
+                  <p style="display: inline-block; justify-content: space-between;" v-if="gp.Postcode">
+                       {{gp.Postcode}}
+                  </p>
+                  <details class="nhsuk-details debug" nhsuk-polyfilled="true" id="nhsuk-details">
+                    <summary class="nhsuk-details__summary" 
+                      role="button" aria-controls="nhsuk-details__text0" tabindex="0" :aria-expanded="bPermissionNoteExapand">
+                      <span class="nhsuk-details__summary-text">Debug section</span>
+                    </summary>
+                    <div class="nhsuk-details__text debug" id="nhsuk-details__text0" aria-hidden="false">
+                        <p class="debug">By clicking on <b><i>This is my GP</i></b>, the Ciziten is about to get all the eligible IAPT Services for the organisation <b>{{ gp.OrganisationName}}</b> having:
+                        <p class="debug">SearchKey: <b>{{gp.SearchKey}}</b></p>
+                        <p class="debug">ODSCode: <b>{{gp.ODSCode }}</b></p>
+                        <p class="debug">Latitude: <b>{{gp.Latitude}}</b></p> 
+                        <p class="debug">Longitude: <b>{{gp.Longitude}}</b></p>                 
+                    </div>
+                  </details>
+                </div>
+                  <p>
+                    <a @click="gpLinkClicked(gp.OrganisationName,gp.Latitude,gp.Longitude)">
+                      <span class="nhsuk-u-visually-hidden">{{ gp.OrganisationName }}</span>
+                      <span aria-hidden="true">This</span> is my GP
+                    </a>
+                  </p>
+                </div>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "FindGPSearchResults",
+  computed: {
+    ...mapState("search", [
+      "gpSearchResults",
+      "gpSearchTerm",
+      "mentalHealthProviderResults"
+    ])
+  },
+  watch: {
+    mentalHealthProviderResults(n) {
+    console.log("mentalHealthProviderResults",n)
+      if (n) {
+        // Results available -> route to display them
+        this.$router.push({ name: "FindMentalHealthResults"});
+      }
+    }
+  },
+  methods: {
+    gpLinkClicked(org, lat, lng) {
+      console.log("gpLinkClicked", org, lat, lng);      
+      this.$store.dispatch("search/getSearchMentalHealthProvidersByCatchment", {
+          lat: lat,
+          lng: lng,
+      });
+    }
+  },
+  data: () => ({
+    bPermissionNoteExapand: false
+  }),
+  mounted() {
+    console.log("Mounted: FindGPSearchResults");
+    if(!this.gpSearchResults) {
+      this.$router.push({ name: "FindGP" });
+    }
+  }
+};
+</script>
+
+<style>
+ a { cursor: pointer; }
+ .debug {font-size: 14px;}
+</style>
---
+++ src/views/FindMentalHealthResults.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="nhsuk-width-container">
+    <main id="main-content" class="nhsuk-main-wrapper">
+      <div class="nhsuk-u-reading-width">
+        <div class="nhsuk-back-link">
+          <router-link to="/find-gp">
+            <svg
+              class="nhsuk-icon nhsuk-icon__chevron-left"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              aria-hidden="true">
+              <path
+                d="M8.5 12c0-.3.1-.5.3-.7l5-5c.4-.4 1-.4 1.4 0s.4 1 0 1.4L10.9 12l4.3 4.3c.4.4.4 1 0 1.4s-1 .4-1.4 0l-5-5c-.2-.2-.3-.4-.3-.7z"
+              > </path>
+            </svg>
+          </router-link>
+        </div>
+        <div v-if="mentalHealthProviderResults">
+          <h1>Contact a psychological therapies service</h1>
+          <h2 class="nhsuk-body-l">
+            <span role="text">
+              <span class="results__count">{{ mentalHealthProviderResults.length }}</span> service(s) are available for location.
+            </span>
+          </h2>
+          <div class="nhsuk-inset-text">
+            <span class="nhsuk-u-visually-hidden">Information: </span>
+            <p>
+              Please note: If your GP is in London, please select the service in
+              the borough where your GP is based.
+            </p>
+            <p>You may not be eligible for the other services being shown.</p>
+            <p>We are working to fix this as soon as possible.</p>
+          </div>
+
+          <details class="nhsuk-details debug" nhsuk-polyfilled="true" id="nhsuk-details">
+            <summary class="nhsuk-details__summary" 
+              role="button" aria-controls="nhsuk-details__text0" tabindex="0" :aria-expanded="bPermissionNoteExapand">
+              <span class="nhsuk-details__summary-text">API Perf</span>
+            </summary>
+            <div class="nhsuk-details__text debug" id="nhsuk-details__text0" aria-hidden="false">
+                <p class="page-perf-debug">API: {{ mentalHealthProviderReqAPI }}</p>
+                <p class="page-perf-debug">Req/Resp: {{ mentalHealthProviderRespPerf }} ms</p>
+            </div>
+          </details>
+
+          <div>
+            <ol class="nhsuk-list inline-list">
+              <li
+                v-for="provider in mentalHealthProviderResults"
+                :key="provider.OrganisationName"
+                class="nhsuk-c-panel results__item"
+                data-index="1"
+              >
+                <hr />
+                <div>
+                  <h3 class="results__name">{{ provider.organisationName }}</h3>
+                  <p class="results__website">
+                    <a>Visit<span class="nhsuk-u-visually-hidden">{{provider.organisationName}}</span> website</a>
+                  </p>
+
+                  <p class="results__telephone">
+                    Telephone:<a href="#" class="telephone_tracking">{{provider.phone}}</a>
+                  </p>
+
+                  <p class="results__email">
+                    Email:<a href="mailto:hillingdontalkingtherapies.cnwl@nhs.net">{{provider.phone}}</a>
+                  </p>
+
+                  <div class="nhsuk-action-link results__self__referral">
+                    <a class="nhsuk-action-link__link" href="#">
+                      <svg
+                        class="nhsuk-icon nhsuk-icon__arrow-right-circle"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        aria-hidden="true"
+                      >
+                        <path d="M0 0h24v24H0z" fill="none"></path>
+                        <path
+                          d="M12 2a10 10 0 0 0-9.95 9h11.64L9.74 7.05a1 1 0 0 1 1.41-1.41l5.66 5.65a1 1 0 0 1 0 1.42l-5.66 5.65a1 1 0 0 1-1.41 0 1 1 0 0 1 0-1.41L13.69 13H2.05A10 10 0 1 0 12 2z"
+                        ></path>
+                      </svg>
+                      <span class="nhsuk-action-link__text"
+                        >Refer yourself online<span
+                          class="nhsuk-u-visually-hidden"
+                          >{{ provider.organisationName }}</span
+                        >
+                      </span>
+                    </a>
+                  </div>
+                  <details class="nhsuk-details debug" nhsuk-polyfilled="true" id="nhsuk-details">
+                    <summary class="nhsuk-details__summary" 
+                      role="button" aria-controls="nhsuk-details__text0" tabindex="0" :aria-expanded="bPermissionNoteExapand">
+                      <span class="nhsuk-details__summary-text">Debug section</span>
+                    </summary>
+                    <div class="nhsuk-details__text debug" id="nhsuk-details__text0" aria-hidden="false">
+                        <p class="debug">The organisation <b>{{ provider.organisationName}} </b>is one of the {{ mentalHealthProviderResults.length }} eligible IAPT Services available having:
+                        <p class="debug">ODSCode: <b>{{ provider.odsCode }}</b></p>
+                        <p class="debug">Latitude: <b>{{ provider.location.position.latitude }}</b></p> 
+                        <p class="debug">Longitude: <b>{{ provider.location.position.longitude }}</b></p>
+                    </div>
+                  </details>
+                </div>
+              </li>
+            </ol>
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  name: "FindMentalHealthResults",
+  computed: {
+    ...mapState("search", [
+      "mentalHealthProviderReqAPI",
+      "mentalHealthProviderRespPerf",
+      "mentalHealthProviderResults"
+    ]),
+  },
+  data: () => ({
+    bPermissionNoteExapand: false
+  }),
+  mounted() {
+    // If no result goto GP input step
+    if(!this.mentalHealthProviderResults) {
+      this.$router.push({ name: "FindGP" });
+    }
+  }
+};
+</script>
+
+<style>
+.page-perf-debug {
+  font-size: 14px;
+  font-weight: 400;
+  margin: 0;
+}
+</style>
