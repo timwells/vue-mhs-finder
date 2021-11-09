53d1e13ba873fded9915ebef8855cb508adb3c58
timwells <timwells018@googlemail.com>
2021-11-09 08:41:15 +0000

    Add explorer idea.

--- src/store/modules/explorer/explorer.js
+++ src/store/modules/explorer/explorer.js
@@ -2,8 +2,7 @@ import axios from "axios";
 import { API_HEADERS, API2 } from "../../common/common" 
 
 const state = {
-  allIAPTResults : null,
-  allGPResults : null
+  allIAPTResults : null
 };
 
 const getters = {
@@ -15,12 +14,6 @@ const mutations = {
       payload.forEach((e,i) => { e.index = i+1; });
     }
     state.allIAPTResults = payload;
-  },
-  SET_GP_SEARCH_RESULTS: (state, payload) => {
-    if(payload) {
-      payload.forEach((e,i) => { e.index = i+1; });
-    }
-    state.allGPResults = payload;
   }
 }
 
@@ -29,8 +22,7 @@ const actions = {
     let reqParameters = {
         // filter: "OrganisationTypeId eq 'CLI'",
         filter: "(Services/any(l: l/ServiceCode eq 'SRV0339')) and (OrganisationTypeId ne 'TRU')",
-        orderby: "OrganisationName",
-        select: "OrganisationName,OrganisationTypeId,Address1,Address2,Address3,City,County,Postcode,Latitude,Longitude,ODSCode,SearchKey,RelatedIAPTCCGs",
+        select: "OrganisationName,OrganisationTypeId,Address1,Address2,Address3,City,County,Postcode,Latitude,Longitude,ODSCode,SearchKey",
         top:500,
         count: true
     };
@@ -39,21 +31,6 @@ const actions = {
       commit("SET_IAPT_SEARCH_RESULTS", resp.data.value);
     });
   },
-
-  postSearchAllGPs({commit}) {
-    let reqParameters = {
-        // filter: "OrganisationTypeId eq 'CLI'",
-        filter: "OrganisationTypeId eq 'GPB'",
-        orderby: "OrganisationName",
-        select: "OrganisationName,OrganisationTypeId,Address1,Address2,Address3,City,County,Postcode,ODSCode,SearchKey,CCG,Latitude,Longitude",
-        top:2000,
-        count: true
-    };
-    commit("SET_GP_SEARCH_RESULTS", null);
-    axios.post(API2, reqParameters, API_HEADERS).then(resp => {
-      commit("SET_GP_SEARCH_RESULTS", resp.data.value);
-    });
-  }
 };
 
 export default {
