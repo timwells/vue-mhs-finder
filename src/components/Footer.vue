8356f6be83d1fcebf3bf953d610d0f3bd6823253
Gabriel Pichiu <gabriel.pichiu1@nhschoices.net>
2021-11-09 18:30:40 +0000

    Clean Header&Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer role="contentinfo">
+    <div class="nhsuk-footer" id="nhsuk-footer">
+      <div class="nhsuk-width-container">
+        <p class="nhsuk-footer__copyright">© Crown copyright</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style></style>
